4a8acb8784e742070f30f5f2368b0a0123a1f5a7
vishalmanes109 <vishalmanes109@gmail.com>
2021-07-10 22:12:42 +0530

    setting : loading screen added

--- src/screens/setting.js
+++ src/screens/setting.js
@@ -3,7 +3,6 @@ import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
 import Avatar from "@material-ui/core/Avatar";
-import PostAddIcon from "@material-ui/icons/PostAdd";
 import InputLabel from "@material-ui/core/InputLabel";
 import FormControl from "@material-ui/core/FormControl";
 import Select from "@material-ui/core/Select";
@@ -11,6 +10,9 @@ import Button from "@material-ui/core/Button";
 import Alert from "@material-ui/lab/Alert";
 import MiniDrawer from "../components/Drawer";
 import SettingsIcon from "@material-ui/icons/Settings";
+import { FormLoader } from "../components/LoadingComponent";
+
+import { getSetting } from "../utilities/ApiService";
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
@@ -45,7 +47,7 @@ const useStyles = makeStyles((theme) => ({
 export default function Setting() {
   const classes = useStyles();
 
-  let [settingData, setsettingData] = useState({
+  let [settingData, setSettingData] = useState({
     email: "",
     password: "",
     currency_id: "",
@@ -55,22 +57,46 @@ export default function Setting() {
   let [error, setError] = useState(false);
   let [success, setSuccess] = useState(false);
   let [message, setMessage] = useState("");
+  let [loading, setLoading] = useState(false);
   const handleInput = (e) => {
     const { id, value } = e.target;
     // ...prevState means it takes prev object and spreads it value .... eg. initial time prevSate object id {name: "",email:"",password:"" }
     // so it got spread as name="", email="", password="" single variable and not an array
 
     // [id]:value => this takes value from e.target.value and put it into e.target.id for every entity in prevState object
-    setsettingData((prevState) => ({
+    setSettingData((prevState) => ({
       ...prevState,
       [id]: value,
     }));
   };
   useEffect(() => {
-    setsettingData(settingData);
+    setLoading(false);
+    setError(false);
+    setMessage("");
+    async function fetchData() {
+      let result = {};
+      setLoading(true);
+      setError(false);
+      setMessage(false);
+      console.log("lol");
+      try {
+        let userid = localStorage.getItem("userid");
+        result = await getSetting(userid);
+        setSettingData(result);
+        console.log("after set: ", result);
+        setError(false);
+        setLoading(false);
+      } catch (error) {
+        console.log("err: ", error);
+        setError(true);
+        setMessage("Server Error, Please Try Again!");
+        setLoading(false);
+      }
+    }
+    fetchData();
   }, []);
   const resetData = () => {
-    setsettingData({ email: "", password: "", currency_id: "", theme: "" });
+    setSettingData({ email: "", password: "", currency_id: "", theme: "" });
   };
   const validateData = () => {
     if (!settingData.email) {
@@ -137,94 +163,101 @@ export default function Setting() {
   };
   const settingComponent = (
     <>
-      <div className={classes.header}>
-        <h3> Settings </h3>
-        <Avatar className={classes.avatar}>
-          <SettingsIcon />
-        </Avatar>
-        {error === true ? (
-          <Alert severity="error">{message}</Alert>
-        ) : (
-          <div></div>
-        )}
-        {success === true ? (
-          <Alert severity="info">{message}</Alert>
-        ) : (
-          <div></div>
-        )}
-      </div>
-      <form
-        className={classes.root}
-        noValidate
-        autoComplete="off"
-        onSubmit={submit}
-      >
-        <Grid className={classes.form} container spacing={3}>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              fullWidth
-              id="email"
-              label="Email"
-              value={settingData.email}
-              helperText="update email"
+      {loading ? (
+        <FormLoader></FormLoader>
+      ) : (
+        <>
+          {" "}
+          <div className={classes.header}>
+            <h3> Settings </h3>
+            <Avatar className={classes.avatar}>
+              <SettingsIcon />
+            </Avatar>
+            {error === true ? (
+              <Alert severity="error">{message}</Alert>
+            ) : (
+              <div></div>
+            )}
+            {success === true ? (
+              <Alert severity="info">{message}</Alert>
+            ) : (
+              <div></div>
+            )}
+          </div>
+          <form
+            className={classes.root}
+            noValidate
+            autoComplete="off"
+            onSubmit={submit}
+          >
+            <Grid className={classes.form} container spacing={3}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  fullWidth
+                  id="email"
+                  label="Email"
+                  value={settingData.email}
+                  helperText="update email"
+                  variant="outlined"
+                  onChange={handleInput}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  fullWidth
+                  id="password"
+                  label="Password"
+                  value={settingData.password}
+                  helperText="update password"
+                  variant="outlined"
+                  onChange={handleInput}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <FormControl variant="outlined" fullWidth>
+                  <InputLabel>Currency</InputLabel>
+                  <Select
+                    native
+                    id="currency_id"
+                    value={settingData.currency_id}
+                    onChange={handleInput}
+                  >
+                    <option aria-label="None" value="" />
+                    <option value={1}>RS</option>
+                    <option value={2}>USD</option>
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <FormControl variant="outlined" fullWidth>
+                  <InputLabel>Theme</InputLabel>
+                  <Select
+                    native
+                    id="theme"
+                    value={settingData.theme}
+                    onChange={handleInput}
+                  >
+                    <option aria-label="None" value="" />
+                    <option value={1}>Dark</option>
+                    <option value={2}>Light</option>
+                  </Select>
+                </FormControl>
+              </Grid>
+            </Grid>
+            <br></br>
+            <Button
+              onClick={submit}
+              type="submit"
               variant="outlined"
-              onChange={handleInput}
-            />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              fullWidth
-              id="password"
-              label="Password"
-              value={settingData.password}
-              helperText="update password"
-              variant="outlined"
-              onChange={handleInput}
-            />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <FormControl variant="outlined" fullWidth>
-              <InputLabel>Currency</InputLabel>
-              <Select
-                native
-                id="currency_id"
-                value={settingData.currency_id}
-                onChange={handleInput}
-              >
-                <option aria-label="None" value="" />
-                <option value={1}>RS</option>
-                <option value={2}>USD</option>
-              </Select>
-            </FormControl>
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <FormControl variant="outlined" fullWidth>
-              <InputLabel>Theme</InputLabel>
-              <Select
-                native
-                id="theme"
-                value={settingData.theme}
-                onChange={handleInput}
-              >
-                <option aria-label="None" value="" />
-                <option value={1}>Dark</option>
-                <option value={2}>Light</option>
-              </Select>
-            </FormControl>
-          </Grid>
-        </Grid>
-        <br></br>
-        <Button
-          onClick={submit}
-          type="submit"
-          variant="outlined"
-          color="primary"
-          // className={classes.submit}
-        >
-          Update
-        </Button>
-        <br></br>
-      </form>
+              color="primary"
+              // className={classes.submit}
+            >
+              Update
+            </Button>
+            <br></br>
+          </form>
+        </>
+      )}
     </>
   );
   return (
